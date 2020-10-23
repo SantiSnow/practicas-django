a7e14435c0419b7360827e7456e703f1725bab22
santiago aguirre perez <aguirresantiago81@gmail.com>
2020-10-23 16:34:49 -0300

    Creadas View y Template para el proyecto

--- proyecto_uno/urls.py
+++ proyecto_uno/urls.py
@@ -20,9 +20,4 @@ from proyecto_uno import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', views.index),
-    path('', views.index),
-    path('about/', views.about),
-    path('contact/', views.contacto),
-    path('perfil/<nombre>', views.usuario),
-    path('edad/<int:edad>', views.edad)
 ]
--- proyecto_uno/views.py
+++ proyecto_uno/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
+from django.template import Template, Context
 import datetime
 
 #views
+#estas son vistas simples creadas como practica, para ver el funcionamiento de las vistas
+"""
 def index(httpRequest):
     return HttpResponse("Bienvenido al index de la aplicacion.")
 
@@ -19,3 +22,20 @@ def usuario(httpRequest, nombre):
 
 def edad(httpRequest, edad):
     return HttpResponse("Su edad es: %s años" %edad)
+
+"""
+
+#vistas creadas con templates
+def index(httpRequest):
+    #carga doc
+    doc = open("C:/Users/Santiago/PycharmProjects/proyectos-django/proyecto_uno/proyecto_uno/templates/index.html")
+    #crea el template
+    template = Template(doc.read())
+    #cierra la carga para limpiar memoria
+    doc.close()
+    #crea el contexto
+    con = Context()
+    
+    vista = template.render(con)
+
+    return HttpResponse(vista)
